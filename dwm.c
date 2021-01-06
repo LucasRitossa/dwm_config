5a9adb9ea43af4a36ff04510ae32f3484271ab9b
Lucas Ritossa <22.lucas.ritossa@westg.org>
2021-01-06 10:54:34 -0500

    restart

--- dwm.c
+++ dwm.c
@@ -210,6 +210,8 @@ static void setup(void);
 static void seturgent(Client *c, int urg);
 static void showhide(Client *c);
 static void sigchld(int unused);
+static void sighup(int unused);
+static void sigterm(int unused);
 static void spawn(const Arg *arg);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
@@ -268,6 +270,7 @@ static void (*handler[LASTEvent]) (XEvent *) = {
 };
 static Atom wmatom[WMLast], netatom[NetLast];
 static int running = 1;
+static int restart = 0;
 static Cur *cursor[CurLast];
 static Clr **scheme;
 static Display *dpy;
@@ -1261,6 +1264,7 @@ propertynotify(XEvent *e)
 void
 quit(const Arg *arg)
 {
+  if(arg->i) restart = 1;
 	running = 0;
 }
 
@@ -1558,6 +1562,8 @@ setup(void)
 
 	/* clean up any zombies immediately */
 	sigchld(0);
+  signal(SIGHUP, sighup);
+  signal(SIGTERM, sigterm);
 
 	/* init screen */
 	screen = DefaultScreen(dpy);
@@ -1659,6 +1665,21 @@ sigchld(int unused)
 		die("can't install SIGCHLD handler:");
 	while (0 < waitpid(-1, NULL, WNOHANG));
 }
+
+void
+sighup(int unused)
+{
+Arg a = {.i = 1};
+quit(&a);
+}
+
+void
+sigterm(int unused)
+{
+Arg a = {.i = 0};
+quit(&a);
+}
+
 
 void
 spawn(const Arg *arg)
@@ -2213,6 +2234,7 @@ main(int argc, char *argv[])
 #endif /* __OpenBSD__ */
 	scan();
 	run();
+  if(restart) execvp(argv[0], argv);
 	cleanup();
 	XCloseDisplay(dpy);
 	return EXIT_SUCCESS;
