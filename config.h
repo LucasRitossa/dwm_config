4fa6f143e1e09762afd41b9a2abef84b5869ad39
Lucas Ritossa <22.lucas.ritossa@westg.org>
2021-07-12 22:46:19 -0400

    .

--- config.h
+++ config.h
@@ -3,28 +3,28 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 20;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int snap      = 0;       /* snap pixel */
+static const unsigned int gappih    = 0;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 0;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "InputMono-Regular:pixelsize=19:antialias=true:autohint=true" };
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:pixelsize=19:antialias=true:autohint=true" };
 
-static const char dmenufont[]       = "InputMono-Regular:pixelsize=19:antialias=true:autohint=true";
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:pixelsize=19:antialias=true:autohint=true";
 //background color
-static const char col_gray1[]       = "#1c1b19"; 
+static const char col_gray1[]       = "#1D2021"; 
 //inactive window border color
-static const char col_gray2[]       = "#444444"; 
+static const char col_gray2[]       = "#282828"; 
 //font color
 static const char col_gray3[]       = "#aaaaaa"; 
 //current tag font color
 static const char col_gray4[]       = "#ffffff"; 
 //top bar color
-static const char col_cyan_bg[]     = "#1c1b19"; 
+static const char col_cyan_bg[]     = "#1D2021"; 
 //selelected border color
 static const char col_cyan[]      = "#30b27a"; 
 static const char *colors[][3]      = {
@@ -35,7 +35,7 @@ static const char *colors[][3]      = {
 
 
 /* tagging */
-static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -43,9 +43,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "flameshot",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "qemu-system-x86_64",    NULL,     NULL,           1,         0,          1,           1,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
@@ -99,7 +99,8 @@ static Key keys[] = {
   { MODKEY|ShiftMask,             XK_r,      spawn,      SHCMD(TERMINAL " -e htop") },
   { MODKEY|ShiftMask,             XK_b,      spawn,      SHCMD(TERMINAL " -e pulsemixer") },
   { MODKEY,                       XK_y,      spawn,      SHCMD(TERMINAL " -e ncmpcpp") },
-  { MODKEY,                       XK_r,      spawn,      SHCMD(TERMINAL " -e ranger") },
+  { MODKEY,                       XK_r,      spawn,      SHCMD(TERMINAL " -e lf") },
+  { MODKEY|ShiftMask,             XK_e,      spawn,      SHCMD("flameshot gui") },
   { MODKEY|ShiftMask,             XK_d,      spawn,      SHCMD("discord") },
   { MODKEY,                       XK_w,      spawn,      SHCMD("firefox") },
 
